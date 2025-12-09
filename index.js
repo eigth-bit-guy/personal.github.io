b9fc31f7627b9d675aaff620cef6dfea85c2bd9e
Sam <157816793+eigth-bit-guy@users.noreply.github.com>
2025-12-09 10:35:07 -0300

    feat: js surprise modal

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const modal = document.getElementById("surpriseModal");
+const btn = document.getElementById("surpriseBtn");
+const span = document.querySelector(".close");
 
+btn.onclick = function () {
+  modal.style.display = "flex";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
