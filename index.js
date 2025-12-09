f4147010d8becf0b74b021f8e675978c4b0b2b15
Sam <157816793+eigth-bit-guy@users.noreply.github.com>
2025-12-09 10:45:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 }
+
+// Texto animado
+const text = "Cada momento ao seu lado é especial... Aqui vão algumas lembranças nossas.";
+let index = 0;
+
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 60);
+  }
+}
+
+btn.onclick = function () {
+  modal.style.display = "flex";
+  document.getElementById("typewriter").innerHTML = "";
+  index = 0;
+  typeWriter();
+}
+
